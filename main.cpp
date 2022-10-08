a84a453697c3fc4ad44ac040b6103f0739556fad
iecetitiz <i.ecetitiz@gmail.com>
2022-10-08 15:28:25 +0300

    added area function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 //author ece
 //date 8th october
 //writing c structure
+
 #include <iostream>
 #include<stdio.h>
-
 #define max 100
+#define PI 3.1415
+
+int r=2;
+void area();
+
+
+class MyClass
+{
+public:
+    int a;
+    void display()
+     { std::cout << "inside class"<<std::endl;
+    }
+};
 
 
 
@@ -12,9 +26,18 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello world!";
-    std::cout << "ece";
-    std::cout << max;
+ MyClass m;
+ m.a = 90;
+ m.display();
+ area();
+    std::cout << "Hello world from main function!"<<std::endl;
+    std::cout << m.a<<std::endl;
+    return 0;
+}
 
-
+void area()
+{
+    float area;
+    area = PI *r *r;
+    std::cout << area<<std::endl;
 }
