e2aa3e60d21b159ceefd1c6b536f86551b8f4fa7
iecetitiz <i.ecetitiz@gmail.com>
2022-10-08 18:28:11 +0300

    boolean variables are added

--- main.cpp
+++ main.cpp
@@ -4,28 +4,40 @@
 
 #include <iostream>
 #include<stdio.h>
-#define max 100
-#define PI 3.1415
+#include<string>
+#define MAX 100 // prefer capital letters
+#define PI 3.1415 //definition is a way to create a constant variable
 
-int r=2;
-void area();
-
+using namespace std;
+int r=2; //global variable
+//void area(); //global function
+const int a = 10; //writing a constant and you can not change the 10 during the program
 
 class MyClass
 {
 public:
     int a;
     void display()
-     { std::cout << "inside class"<<std::endl;
+     { std::cout<< "inside class"<<std::endl;
     }
 };
 
-
-
-using namespace std;
+void area()
+{
+    float area;
+    area = PI *r *r;
+    std::cout << area<<std::endl;
+}
 
 int main()
 {
+    char ch = 'a';
+    bool bl = true;
+    std::cout << (int)bl<<std::endl;//char'i int'e cast edebiliyorum
+    std::cout << (int)a<<std::endl;
+    const string ece = "ece"; // i can create a const in main function
+    std::cout<<ece<<std::endl;
+
  MyClass m;
  m.a = 90;
  m.display();
@@ -35,9 +47,4 @@ int main()
     return 0;
 }
 
-void area()
-{
-    float area;
-    area = PI *r *r;
-    std::cout << area<<std::endl;
-}
+
